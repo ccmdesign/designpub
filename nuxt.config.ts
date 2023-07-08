8d07a39b313e291e7c8a3820c052a7c5259465d9
Claudio <claudio@ccmdesign.ca>
2023-07-08 10:07:45 -0700

    nuxt initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,48 @@
+import { defineNuxtConfig } from "nuxt/config";
+import { resolve } from "path";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  meta: {
+    title: "Boilerplate DS",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+    script: [],
+    link: [
+      // { rel: "stylesheet", href: "https://use.typekit.net/tsd2tcy.css" },
+      // { rel: "stylesheet", href: "https://cloud.typography.com/7264818/7115832/css/fonts.css" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined",
+      },
+    ],
+  },
+  css: [
+    '@ccmdesign/ccm-ds/assets/css/main.scss',
+    'public/assets/theme.css',
+    'public/assets/styles.css',
+  ],
+  components: {
+    "dirs": [
+      {
+        "path": resolve(__dirname, 'node_modules/@ccmdesign/ccm-ds/ccm-ds/03-objects'),
+        "global": true
+      },
+      {
+        "path": resolve(__dirname, 'node_modules/@ccmdesign/ccm-ds/ccm-ds/04-components'),
+        "global": true
+      },
+      {
+        "path": resolve(__dirname, 'node_modules/@ccmdesign/ccm-ds/ccm-ds/05-modules'),
+        "global": true
+      },
+      {
+        "path": resolve(__dirname, 'node_modules/@ccmdesign/ccm-ds/ccm-ds/06-sections'),
+        "global": true
+      },
+      "~/components"
+    ]
+  },
+});
