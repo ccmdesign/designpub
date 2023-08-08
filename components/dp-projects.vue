0e03659279f7cfe021c41a87ce8f2522737b9af7
Claudio <claudio@ccmdesign.ca>
2023-08-08 09:55:58 -0700

    copy changes

--- components/dp-projects.vue
+++ components/dp-projects.vue
@@ -2,7 +2,7 @@
   <base-section color="base" size="l">
     <center-l size="wide">
       <h2 class="dp-h3 | color:white padding-bottom:s1">Recent Work</h2>
-      <h3 class="dp-tagline | color:accent margin-bottom:s2">DesignPub is an initiative by <a href="https://www.ccmdesign.ca" target="_blank">CCM Design</a>. We've been working with research institutions for 17 years, and from this experience and knowledge of the space we decided to launch this service offering.</h3>
+      <h3 class="dp-tagline | color:accent margin-bottom:s2">DesignPub is an initiative by <a href="https://www.ccmdesign.ca" target="_blank">CCM Design</a>. Our team brings more than 17 years of experience and knowledge in this field. </h3>
     </center-l>
     <reel-l reelHeight="400px" space="0" itemWidth="400px" class="portfolio">
       <li v-for="i in portfolio">
